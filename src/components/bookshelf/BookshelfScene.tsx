7b751a3d763a5a5ce60a4e640366fb50768a7700
Gustavo Barbosa <abguto.06@gmail.com>
2026-03-09 18:07:03 -0300

    sabomba não funciona

--- src/components/bookshelf/BookshelfScene.tsx
+++ src/components/bookshelf/BookshelfScene.tsx
@@ -1,58 +1,119 @@
 "use client";
 
+import gsap from "gsap";
 import { Book } from "./book";
 import { ScrollIndicator } from "./scroll-indicator";
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
+
+const BOOKS = [
+  {
+    title: "Teste",
+    color: "#2d5a3d",
+    height: 180,
+    width: 34,
+  },
+  {
+    title: "Teste",
+    color: "#4a5a2d",
+    height: 210,
+    width: 30,
+  },
+  {
+    title: "Historia",
+    color: "#8b4a30",
+    height: 162,
+    width: 34,
+    isMemoryBook: true,
+  },
+  {
+    title: "Teste",
+    color: "#3d2d5a",
+    height: 190,
+    width: 32,
+  },
+  {
+    title: "Teste",
+    color: "#008000",
+    height: 200,
+    width: 33,
+  },
+  {
+    title: "Teste",
+    color: "#0083F0",
+    height: 156,
+    width: 24,
+  },
+];
 
 export function BookshelfScene() {
-  const coverRef = useRef<HTMLDivElement>(null);
-  const pageRefs = useRef<(HTMLDivElement | null)[]>([]);
+  const containerRef = useRef<HTMLDivElement>(null);
+  const titleRef = useRef<HTMLDivElement>(null);
+  const booksRowRef = useRef<HTMLDivElement>(null);
+  const memoryBookRef = useRef<HTMLDivElement>(null);
+  const othersBooksRefs = useRef<(HTMLDivElement | null)[]>([]);
 
-  const BOKS = [
-    {
-      title: "Teste",
-      color: "#2d5a3d",
-      height: 180,
-      width: 34,
-    },
-    {
-      title: "Teste",
-      color: "#4a5a2d",
-      height: 210,
-      width: 30,
-    },
-    {
-      title: "Historia",
-      color: "#8b4a30",
-      height: 162,
-      width: 34,
-      isMemoryBook: true,
-    },
-    {
-      title: "Teste",
-      color: "#3d2d5a",
-      height: 190,
-      width: 32,
-    },
-    {
-      title: "Teste",
-      color: "#008000",
-      height: 200,
-      width: 33,
-    },
-    {
-      title: "Teste",
-      color: "#0083F0",
-      height: 156,
-      width: 24,
-    },
-  ];
+  useEffect(() => {
+    console.log("outros antes: ", othersBooksRefs.current);
+
+    const ctx = gsap.context(() => {
+      gsap.from(titleRef.current, {
+        opacity: 0,
+        y: 20,
+        duration: 1,
+        delay: 0.2,
+        ease: "power2.out",
+      });
+
+      const bookQueue = booksRowRef.current?.children;
+
+      if (bookQueue) {
+        gsap.from(Array.from(bookQueue), {
+          opacity: 0,
+          y: 20,
+          duration: 0.5,
+          delay: 0.1,
+          ease: "power2.out",
+          stagger: 0.2,
+        });
+      }
+
+      const tl = gsap.timeline({
+        scrollTrigger: {
+          trigger: containerRef.current,
+          start: "top top",
+          end: "bottom bottom",
+          scrub: 1.2,
+          invalidateOnRefresh: true,
+        },
+      });
+
+      const othersBooks = othersBooksRefs.current.filter(
+        Boolean,
+      ) as HTMLDivElement[];
+
+      tl.to(
+        othersBooks,
+        {
+          opacity: 0,
+          duration: 0.08,
+          stagger: 0.005,
+          ease: "power1.out",
+        },
+        0.06,
+      );
+
+      console.log("outros depois: ", othersBooksRefs.current);
+    }, containerRef);
+
+    return () => ctx.revert();
+  }, []);
 
   return (
-    <div className="relative w-full h-[800vh]">
+    <div ref={containerRef} className="relative w-full h-[800vh]">
       <div className="sticky top-0 h-screen flex flex-col justify-center items-center overflow-hidden">
         <div className="absolute inset-x-0 top-30 flex flex-col gap-1 items-center">
           <h1
+            ref={titleRef}
             className="text-[#3E3232] font-bold text-5xl"
             style={{ fontFamily: "Georgia" }}
           >
@@ -63,18 +124,29 @@ export function BookshelfScene() {
           </p>
         </div>
 
-        <div className="flex items-end gap-1">
-          {BOKS.map((b, i) => {
+        <div ref={booksRowRef} className="flex items-end gap-1">
+          {BOOKS.map((b, i) => {
+            if (b.isMemoryBook) {
+              return (
+                <div key={i} ref={memoryBookRef}>
+                  <Book {...b} isMemoryBook />
+                </div>
+              );
+            }
             return (
-              <Book
-                title={b.title}
-                color={b.color}
-                height={b.height}
-                width={b.width}
+              <div
                 key={i}
-              />
+                ref={(ob) => {
+                  const otherIndex = i > 2 ? i - 1 : i;
+                  othersBooksRefs.current[otherIndex] = ob;
+                }}
+              >
+                <Book {...b} />
+              </div>
             );
           })}
+
+          {/* othersBooksRefs */}
         </div>
         <div className="bg-[#75502B] h-8 w-5/6 rounded-b-sm shadow-[0_4px_12px_rgba(0,0,0,0.4),inset_0_1px_0_rgba(255,255,255,0.15),inset_0_-1px_0_rgba(0,0,0,0.3)]" />
 
